e9d54d72e743cd63d38b232a7ed9f41e288d6af5
kushiben8-ctrl <kushiben8@gmail.com>
2025-12-30 10:43:06 +0000

    variables,objects

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,46 @@
+//---- first lecture- var , const, let---- //
+/* console.log ("khushi")
+const accountId = 123321
+let accountEmail = "kushiben8@gmail.com"
+var accountPassward = " 346556"
+ /*
+prefer not to use var 
+reason : becase the issue in scope and in functional scope  
+/*
+accountCity = "ahemdabad ";
+  let accountState;
+
+
+
+// accountId =2; // not allowed 
+accountEmail= "sahil8@gmail.com"
+accountPassward =" 0-399038599"
+accountCity = "delhi"
+ console.table([accountEmail, accountId, accountCity,accountPassward,accountState])
+
+*/
+//---second lec- data type ---//
+
+"use strict";
+// treat all JS code as a newer version[ new syntax ] //
+// alert("hello");// using browser 
+// console.log("hello " + " khushi")
+
+
+// let name ="khushi"
+// let age = 18
+// let isLoggedIn = false ;
+
+//number => 2 to the powe 53 oe 52 
+// bigint 
+// string => " "
+// boolean => true or false 
+// null => stand alone value 
+//undefined =>  variable value is not assinged 
+// symbol =>  uniqeness 
+
+
+
+// object 
+
+ console.log(typeof "khushi" );
