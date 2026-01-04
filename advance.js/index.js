f4fb1a419fe6ad39d861eae4c5b0d7105cd647f0
TATAD KHUSHI <kushiben8@gmail.com>
2026-01-04 07:35:31 +0000

    DATA OBJECT MODUAL

---
+++ advance.js/index.js
@@ -0,0 +1,62 @@
+// const parent = document.querySelector('.parent');
+// //  console.log(parent);
+// //   console.log(parent.children[1].innerHTML);
+
+
+
+
+
+// // for (let i = 0; i < parent.children.length; i++) {
+// //   console.log( parent.children[i].innerHTML)
+
+// // }
+
+// parent.children[1].style.color = "orange"
+// // console.log(parent.firstElementChild);
+// // console.log(parent.lastElementChild);
+
+// const day1 = document.querySelector('.day')
+// // console.log(day1);
+
+// // console.log(day1.parentElement);
+
+// // console.log(day1.nextElementSibling);
+// console.log("NODES:", 
+//     parent.childNodes );
+// // console.log(parent.childNodes)
+
+
+
+
+//
+
+// const div = document.createElement('div')
+// console.log(div)
+// div.className = "main"
+// div.id = Math.round(Math.random() * 10 + 1)
+// div.setAttribute("title", "genrate title ")
+// div.style.backgroundColor = "green"
+// div.style.padding = "20px"
+// // const inner = div.innerText ="chai or code"
+// // console.log(inner)
+// const add = document.createTextNode("chai or code ")
+// div.appendChild(add)
+
+// document.body.appendChild(div)
+
+
+// function addLanguage (langName){
+//     const li = document.createElement('li');
+//     li.innerHTML = `%{langName}`
+//     document.querySelector('.language').appendChild(li)
+
+//     }
+
+// addLanguage("python")
+// function addOptiLanguage(langName) {
+//     const li = document.createElement('li');
+//     li.appendChild(document.createTextNode(langName));
+//     document.querySelector('.language').appendChild(li);
+// }
+// addOptiLanguage("python")
+
